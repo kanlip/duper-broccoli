11da158431efae9cd1954c8d7596a52d8ec95e20
HuiChinOh <56510299+HuiChinOh@users.noreply.github.com>
2020-07-29 11:59:37 +0800

    Add in the GUI and some test scripts
    Added the Hpbar and exp bar and a hpbar manager script
    Added in the mobile GUI and a test playermovement script for mobile
    Added in the script to set active for mobile GUI when player is playing on mobile
    Added in the  GUI to pause the game
    Added the settings GUI and panel

---
+++ CSCI356 Project/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Slider hpSlider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setMaxHp(int health)
+    {
+        hpSlider.maxValue = health;
+        hpSlider.value = health;
+    }
+
+    public void setHp(int health)
+    {
+        hpSlider.value = health;
+    }
+}
---
+++ CSCI356 Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    protected Joystick joystick;
+    protected AttackButtonManager attackButton;
+    
+    public float movementSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        attackButton = FindObjectOfType<AttackButtonManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidBody = GetComponent<Rigidbody>();
+
+        float mvX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        float mvZ = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        transform.Translate(mvX, 0, mvZ);
+
+
+#if UNITY_ANDROID || UNITY_IOS
+
+         rigidBody.velocity = new Vector3(joystick.Horizontal * movementSpeed, 
+         rigidBody.velocity.y, joystick.Vertical * movementSpeed);
+
+#endif
+    }
+}
