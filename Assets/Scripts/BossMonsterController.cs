86786c10063c6bec7a303b31f352a42133a6b354
kanlip <kan_serv@outlook.com>
2020-08-07 22:03:56 +0700

    Added Animation and model

--- Assets/Scripts/BossMonsterController.cs
+++ Assets/Scripts/BossMonsterController.cs
@@ -18,6 +18,7 @@
         }
         public void Attack()
         {
+            
             throw new NotImplementedException();
         }
 
--- Assets/Scripts/MediumMonsterController.cs
+++ Assets/Scripts/MediumMonsterController.cs
@@ -1,25 +1,50 @@
 using System;
 using UnityEngine;
 
-public class MediumMonsterController:MonoBehaviour,IEnemy
+public enum EnemyAnimation
 {
+    Attack,
+    Run,
+    Dead,
+    Idle
+    
+}
+public class MediumMonsterController:MonoBehaviour,IEnemy
+{   
     public int Health { get; set; }
     public int Exp { get; set; }
     public int MovementSpeed { get; set; }
     public int AttackSpeed { get; set; }
     private int CurrentHealth { get; set; }
+    private GameObject player;
+    private Transform playerTransform;
+    private Animator enemyAnimator;
     
-        
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = playerTransform.GetComponent<Transform>();
+        enemyAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            Attack();
+    }          
+
     private void Awake()
     {
         Health = 150;
         Exp = 5;
         CurrentHealth = Health;
     }
-
+    
     public void Attack()
     {
-        throw new NotImplementedException();
+        enemyAnimator.SetTrigger(EnemyAnimation.Attack.ToString());
+        
     }
 
     public void DamageTaken(int damageTaken)
