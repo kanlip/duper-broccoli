d8141c00f44bf484d8de1d837535627c1c533667
kanlip <kan_serv@outlook.com>
2020-08-10 14:29:11 +0700

    Added Small and Boss monster on different size

--- Assets/Scripts/IEnemy.cs
+++ Assets/Scripts/IEnemy.cs
@@ -7,6 +7,7 @@ public interface IEnemy
     int Health { get; set; }
     int Exp { get; set; }
     int MovementSpeed { get; set; }
+    EnemyAnimation CurrentBotState { get; set; }
     int AttackSpeed { get; set; }
     void DamageTaken(int damageTaken);
     void Attack();
--- Assets/Scripts/MediumMonsterController.cs
+++ Assets/Scripts/MediumMonsterController.cs
@@ -1,38 +1,64 @@
 using System;
 using UnityEngine;
 
-public enum EnemyAnimation
-{
-    Attack,
-    Run,
-    Dead,
-    Idle
-    
-}
+
 public class MediumMonsterController:MonoBehaviour,IEnemy
 {   
     public int Health { get; set; }
     public int Exp { get; set; }
     public int MovementSpeed { get; set; }
+    public EnemyAnimation CurrentBotState { get; set; }
     public int AttackSpeed { get; set; }
     private int CurrentHealth { get; set; }
-    private GameObject player;
-    private Transform playerTransform;
-    private Animator enemyAnimator;
+    private GameObject _player;
+    private Transform _playerTransform;
+    private Animator _enemyAnimator;
     
     
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerTransform = playerTransform.GetComponent<Transform>();
-        enemyAnimator = GetComponent<Animator>();
+        _enemyAnimator = GetComponent<Animator>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        if(_player != null)
+            _playerTransform = _playerTransform.GetComponent<Transform>();
+        
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha0))
             Attack();
-    }          
+        if (_playerTransform == null)
+        {
+            _enemyAnimator.SetTrigger(EnemyAnimation.Idle.ToString());
+            return;
+        }
+
+        var direction = _playerTransform.position - transform.position;
+        var angleOfView = Vector3.Angle(direction, transform.forward);
+        
+        if (direction.magnitude < 10 && angleOfView < 40)
+        {
+            direction.y = 0;
+            transform.rotation = Quaternion.LookRotation(direction);
+            if (direction.magnitude < 1)
+            {
+                Attack();
+                transform.Translate(0,0,0.04f);
+            }
+            else
+            {
+                _enemyAnimator.SetTrigger(EnemyAnimation.Run.ToString());
+                transform.Translate (0, 0, 0.2F);
+                CurrentBotState = EnemyAnimation.Run;
+            }
+        }
+        else
+        {
+            _enemyAnimator.SetTrigger(EnemyAnimation.Idle.ToString());
+            CurrentBotState = EnemyAnimation.Idle;
+        }
+    }
 
     private void Awake()
     {
@@ -43,13 +69,18 @@ public class MediumMonsterController:MonoBehaviour,IEnemy
     
     public void Attack()
     {
-        enemyAnimator.SetTrigger(EnemyAnimation.Attack.ToString());
+        _enemyAnimator.SetTrigger(EnemyAnimation.Attack.ToString());
+        
+        CurrentBotState = EnemyAnimation.Attack;
     }
     
     public void DamageTaken(int damageTaken)
     {
-        if(CurrentHealth > 0) CurrentHealth -= damageTaken;
-        else enemyAnimator.SetTrigger(EnemyAnimation.Dead.ToString());
+        if(CurrentHealth > 0) 
+            CurrentHealth -= damageTaken;
+        else 
+            _enemyAnimator.SetTrigger(EnemyAnimation.Dead.ToString());
     }
+    
 }
 
