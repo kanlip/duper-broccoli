531803d7a5a8faacbeb6a539aaee894426a45972
xelycx <xelycx@gmail.com>
2020-08-24 16:28:53 +0800

    Clean up project and camera, shoot fix
    pls dont import unused stuff

---
+++ Assets/Scripts/TPSCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Cameras;
+
+public class TPSCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject pivotGO = new GameObject("Pivot");
+        if (pivotGO)
+        {
+            pivotGO.transform.position = new Vector3(1,2,-3);
+            pivotGO.transform.SetParent(transform, false);
+        }
+        Camera cam = Camera.main;
+        cam.transform.position = new Vector3(0, 1, 0);
+        cam.transform.SetParent(GameObject.Find("Pivot").GetComponent<Transform>(), false);
+
+        gameObject.AddComponent<FreeLookCam>();
+        gameObject.AddComponent<ProtectCameraFromWallClip>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
